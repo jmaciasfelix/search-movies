07a7dda80e6b2100169c885f3ee79daae3236627
Jesús Macías Félix <jmacias@atsistemas.com>
2020-12-11 17:02:12 +0100

    Logic login, create inputs and hidden password

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+//wouter
+import { Link } from "wouter";
 
-export const Login = () => (
-    <section>
-        <h1>Este componente es el Login</h1>
-    </section>
-)
+const Wrapper = styled.section`
+  border: 1px solid red;
+  height: 100%;
+
+  p {
+    margin: 0;
+    padding: 0;
+    font-size: 14px;
+    text-align: right;
+    cursor: pointer;
+  }
+  label {
+    font-size: 14px;
+  }
+  i {
+    cursor: pointer;
+  }
+`;
+const FormLogin = styled.form`
+  //border: 1px solid blue;
+  margin-top: 10%;
+  padding: 10px 40px;
+  input {
+    display: block;
+    width: 100%;
+    padding: 5px 2px;
+  }
+  button {
+    margin-right: 0;
+    float: right;
+  }
+`;
+const FieldPassword = styled.div``;
+
+export const Login = () => {
+  const [isHidden, setHidden] = useState(false);
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState("");
+
+  const isDisabled = () => !(user && password);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log("handleLogin");
+  };
+
+  return (
+    <Wrapper>
+      <Link>
+        <p>Register</p>
+      </Link>
+      <FormLogin>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            name="username"
+            placeholder="Enter Username"
+            value={user}
+            onChange={(e) => setUser(e.target.value)}
+          />
+        </div>
+        <FieldPassword>
+          <label htmlFor="lname">Password</label>
+          <input
+            type={isHidden ? "password" : "text"}
+            name="password"
+            placeholder="Enter Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <i type="button" onClick={() => setHidden(!isHidden)}>
+            👀
+          </i>
+        </FieldPassword>
+        <button type="submit" onClick={handleLogin} disabled={isDisabled()}>
+          Login
+        </button>
+      </FormLogin>
+    </Wrapper>
+  );
+};
